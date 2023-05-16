71eb20a5641f395cde00cff42e700f1c2b3cefc7
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-05-16 00:00:13 -0300

    aula08

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,12 +19,38 @@
         
         
     </head>
-   @yield('content')
+   
         
     
     <body>
-       
 
+        <header>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="/"><img class="imgMenu" src="/img/hdcevents_logo.svg" alt="">Eventos</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Criar Eventos</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Entrar</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Cadastrar</a>
+                      </li>
+                      
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            
+        </header>
+       
+        @yield('content')
         <footer>
             <p>HDC Eventss &copy; 2020</p>
         </footer>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,4 +4,12 @@
     
 
 <h1>Tela de Produtos</h1>
+
+@if($busca != '') 
+
+<p>O usuario está buscando por {{$busca}}</p>
+
+@endif
+
+
 @endsection
